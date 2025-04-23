182f8f272617392d17b2c749ac4d3340990300bf
brendaharriet <nalwangamary2020@gmail.com>
2025-04-23 14:04:26 +0300

    Update Test.sql

--- DBMS TEST/Test.sql
+++ DBMS TEST/Test.sql
@@ -1,33 +1,36 @@
--- a. i
--- CREATE DATABASE finance;
-Create table Finance;
-Insert into (StudentName, StudentID, Birthdate, Year, MobileNo)
-values('Jane', 'Nichola', 'Cyrus', 'Neema', 'Caroline', 'Judith', 'Grace') 
-(235, 26, 1, 2, 3, 29, 127)
-(12/1/1987, 22/7/2002, 15/6/1989, 29/11/2008, 18/10/2002, 17/6/2003, 14/1/200)
-('Y1S2','Y1S1','Y2S1', 'Y2S1', 'Y1S1', 'Y4S2', 'Y1S2')
-(0722245321, 07734467834, 0789563425, 0975634257, 0856273548,0743425227, 072535353537)
-(;
-
-
--- ii
-
-select * 
-from finance
-WHERE Year = Y1S1 AND tuition_fee > 0;
-
--- iii
-select * 
-from finance
-WHERE StudentName AND StudentID AND Tuition_fee > 0;
-
-
-USE StudentName, StudentID,;
-SELECT * 
-FROM Customers;
-
-select * 
-from finance
-WHERE StudentName = Y1S1 AND tuition_fee > 0;
-
-
+ -- CREATE DATABASE finance;
+ -- USE finance;
+ -- i
+ -- CREATE TABLE Finance (
+ --     StudentID INT  PRIMARY KEY,
+ --     StudentName VARCHAR(255),
+ --     Birthdate DATE,
+ --     Year VARCHAR(10) ,
+ --     MobileNo VARCHAR(255) ,
+ 
+ -- );
+ 
+ -- ii
+ -- SELECT StudentName, TuitionFee
+ -- FROM Finance
+ -- WHERE Year = 'Y1S1' AND TuitionFee > 0;
+ 
+ -- iii
+ -- SELECT StudentID, Year, TuitionFee
+ -- FROM Finance
+ -- WHERE TuitionFee >= 30000;
+ 
+ -- iv
+ -- INSERT INTO Finance (StudentID, StudentName, Birthdate, Year, MobileNo, TuitionFee, AdministrationFee, ExamsFee, MedicalFee)
+ -- VALUES (242, 'Dan', '2001-04-20', 'Y1S2', '0723456123', 0, 0, 0, 0);
+ 
+ -- v
+ -- UPDATE Finance
+ -- SET StudentName = 'Nelson'
+ -- WHERE StudentID = 236;
+ 
+ -- vi
+ SELECT
+     MAX(TuitionFee) AS 'Highest Amount',
+     MIN(TuitionFee) AS 'Lowest Amount'
+ FROM Finance;
