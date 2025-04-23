a7f7094d550a4a1e44decfeb3178b11a4cc0979c
brendaharriet <nalwangamary2020@gmail.com>
2025-04-23 11:57:13 +0300

    Add files via upload

---
+++ DBMS TEST/Test.sql
@@ -0,0 +1,33 @@
+-- a. i
+-- CREATE DATABASE finance;
+Create table Finance;
+Insert into (StudentName, StudentID, Birthdate, Year, MobileNo)
+values('Jane', 'Nichola', 'Cyrus', 'Neema', 'Caroline', 'Judith', 'Grace') 
+(235, 26, 1, 2, 3, 29, 127)
+(12/1/1987, 22/7/2002, 15/6/1989, 29/11/2008, 18/10/2002, 17/6/2003, 14/1/200)
+('Y1S2','Y1S1','Y2S1', 'Y2S1', 'Y1S1', 'Y4S2', 'Y1S2')
+(0722245321, 07734467834, 0789563425, 0975634257, 0856273548,0743425227, 072535353537)
+(;
+
+
+-- ii
+
+select * 
+from finance
+WHERE Year = Y1S1 AND tuition_fee > 0;
+
+-- iii
+select * 
+from finance
+WHERE StudentName AND StudentID AND Tuition_fee > 0;
+
+
+USE StudentName, StudentID,;
+SELECT * 
+FROM Customers;
+
+select * 
+from finance
+WHERE StudentName = Y1S1 AND tuition_fee > 0;
+
+
